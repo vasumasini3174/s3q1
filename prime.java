9f0438686a940fd601131b943ad1406deeed7064
vasumasini3174 <vasumasini17@gmail.com>
2024-11-27 13:41:15 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class prime {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        //Scanner sc=new Scanner(System.in);
+        int n=5;
         int count=0;
         for(int i=2;i<=n;i++){
             if(n%i==0){
